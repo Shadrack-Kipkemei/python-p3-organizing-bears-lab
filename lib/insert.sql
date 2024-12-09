50b465f6342c15ccab236388cde573b9f404fddf
sktanui <shadrackkemei2019@gmail.com>
2024-12-09 15:31:09 +0300

    pass tests and complete leb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color) VALUES ("Mr.Chocolate",3,"M","chocolate");
+INSERT INTO bears (name,age,sex,color) VALUES ("Rowdy",5,"M","white");
+INSERT INTO bears (name,age,sex,color) VALUES ("Tabitha",7,"F","brown");
+INSERT INTO bears (name,age,sex,color) VALUES ("Sergeant Brown",4,"M","brown");
+INSERT INTO bears (name,age,sex,color) VALUES ("Melissa",3,"F","black");
+INSERT INTO bears (name,age,sex,color) VALUES ("Grinch",6,"M","Green");
+INSERT INTO bears (name,age,sex,color) VALUES ("Wendy",8,"F","white");
+INSERT INTO bears (name,age,sex,color) VALUES (NULL,5,"M","black");
